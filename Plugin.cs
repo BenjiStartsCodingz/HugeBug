f3a6641e0f97a1480f673b7e7307bd898c2193b8
BenjiGTAGMODS <100496712+BenjiStartsCodingz@users.noreply.github.com>
2023-08-03 18:26:10 -0500

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using GorillaNetworking;
+using GorillaLocomotion;
+using PlayFab;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using PlayFab.ClientModels;
+using Photon.Pun;
+using Photon.Realtime;
+using PlayFab.Internal;
+using Steamworks;
+using MelonLoader;
+using System.Numerics;
+
+namespace HugeBug
+{
+
+    public class HugeBug_1 : MonoBehaviour
+    {
+        void Bug()
+            {
+            GameObject floatingBug = GameObject.Find("Floating Bug"); // i made this mod dont steal
+            Transform bugTransform = floatingBug.GetComponent<Transform>();
+
+            if (bugTransform != null)
+            {
+                bugTransform.localScale = new UnityEngine.Vector3(15f, 15f, 15f);
+            }
+        }
+        }
+    }
+
+
---
+++ PluginInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigMOTD
+{
+    class PluginInfo
+    {
+        public const string GUID = "com.Benjii.gorillatag.hugebug";
+        public const string Name = "HugeBug";
+        public const string Version = "1.0.0";
+    }
+}
